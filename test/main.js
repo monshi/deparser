6c6d09c98d93b4087afbd229961301d3633cb4b2
Omid Monshizadeh <monshio@gmail.com>
2018-09-13 23:06:18 -0700

    improved tests and added tests for graph

--- test/main.js
+++ test/main.js
@@ -1,6 +1,7 @@
 const {expect, assert} = require('chai');
 const Deparser = require('../lib/Deparser');
 const treeJSON = require('./fixture/tree.json');
+const graphJSON = require('./fixture/graph.json');
 
 // Test suite
 describe('Deparser dependency tests', () => {
@@ -31,13 +32,13 @@ describe('Deparser dependency tests', () => {
 
   it('Dependency tree to include run time dependencies', () => {
     const dependencyTree = deparserInstance.getDependencyTree();
-    const dependencies = dependencyTree.filter(dep => dep.type === 'dependencies');
+    const dependencies = dependencyTree.filter(dep => dep.dependencyType === 'dependencies');
     expect(dependencies).to.have.lengthOf(2);
   });
 
   it('Dependency tree to include dev dependencies', () => {
     const dependencyTree = deparserInstance.getDependencyTree();
-    const dependencies = dependencyTree.filter(dep => dep.type === 'devDependencies');
+    const dependencies = dependencyTree.filter(dep => dep.dependencyType === 'devDependencies');
     expect(dependencies).to.have.lengthOf(1);
     expect(dependencies[0].name).to.equal('mocha');
     expect(dependencies[0].version).to.equal('5.2.0');
@@ -45,7 +46,34 @@ describe('Deparser dependency tests', () => {
 
   it('Dependency tree to include optional dependencies', () => {
     const dependencyTree = deparserInstance.getDependencyTree();
-    const dependencies = dependencyTree.filter(dep => dep.type === 'optionalDependencies');
+    const dependencies = dependencyTree.filter(dep => dep.dependencyType === 'optionalDependencies');
+    expect(dependencies).to.have.lengthOf(1);
+    expect(dependencies[0].name).to.equal('fsevents');
+    expect(dependencies[0].version).to.equal('1.2.4');
+  });
+
+  it('Full dependency graph test', () => {
+    const dependencyGraph = deparserInstance.getDependencyGraph();
+    assert.deepEqual(dependencyGraph, graphJSON);
+  });
+
+  it('Dependency graph to include run time dependencies', () => {
+    const dependencyGraph = deparserInstance.getDependencyGraph();
+    const dependencies = dependencyGraph.modules.filter(dep => dep.dependencyType === 'dependencies');
+    expect(dependencies).to.have.lengthOf(2);
+  });
+
+  it('Dependency graph to include dev dependencies', () => {
+    const dependencyGraph = deparserInstance.getDependencyGraph();
+    const dependencies = dependencyGraph.modules.filter(dep => dep.dependencyType === 'devDependencies');
+    expect(dependencies).to.have.lengthOf(1);
+    expect(dependencies[0].name).to.equal('mocha');
+    expect(dependencies[0].version).to.equal('5.2.0');
+  });
+
+  it('Dependency graph to include optional dependencies', () => {
+    const dependencyGraph = deparserInstance.getDependencyGraph();
+    const dependencies = dependencyGraph.modules.filter(dep => dep.dependencyType === 'optionalDependencies');
     expect(dependencies).to.have.lengthOf(1);
     expect(dependencies[0].name).to.equal('fsevents');
     expect(dependencies[0].version).to.equal('1.2.4');
